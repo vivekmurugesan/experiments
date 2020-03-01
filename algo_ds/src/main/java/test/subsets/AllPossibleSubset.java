91e6debd373c9f1fdb9b80041e6e2cb820d43021
Vivek Murugesan <vivek.murugesan@gmail.com>
2020-03-01 19:55:49 +0400

    For all possible subsets.

--- algo_ds/src/main/java/test/subsets/AllPossibleSubset.java
+++ algo_ds/src/main/java/test/subsets/AllPossibleSubset.java
@@ -1,7 +1,9 @@
 package test.subsets;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 
@@ -10,33 +12,109 @@ import java.util.List;
  */
 public class AllPossibleSubset {
 
-	private static int[] test = {1,2,3,4};
+	private static int[] productIds = {1,2,3,4,5,6};
+	
+	private static int[] prices = {5,10,15,20,25,30};
+	
+	//private static int[] test = {1,2,4,8,16,32,64};
 	
 	public static void main(String[] args) {
 		
+		//System.out.println("..." + (5 & 3));
+		
 		AllPossibleSubset subsets = new AllPossibleSubset();
-		subsets.computeAllSubsets(test);
+		subsets.computeAllSubsets(productIds, prices);
 	}
 	
-	public void computeAllSubsets(int[] test) {
-		int n = test.length;
+	public void computeAllSubsets(int[] productIds, int[] prices) {
+		int n = productIds.length;
 		int subsetSize = (int) Math.pow(2, n);
 		
+		Map<Integer, List<Integer>> setCache = new HashMap<>();
+		Map<Integer, Integer> setSumCache = new HashMap<>();
+		
 		for(int i=0;i<subsetSize;i++) {
-			List<Integer> subset = getSetWithIndex(test, i);
-			System.out.println("subset: " + i + "::"+ subset);
+			getSetWithIndexOptimized(productIds, prices, i, setCache, setSumCache);
+			
+			List<Integer> subset = setCache.get(i);
+			int setSum = setSumCache.get(i);
+			System.out.println("subset: " + i + "::"+ subset+ " :: sum: " + setSum 
+					+ ", id::" + encode(subset) + ".. decode test::" + decode(i, n) );
 		}
 	}
 	
-	public List<Integer> getSetWithIndex(int[] test, int index) {
-		List<Integer> subset = new ArrayList<>();
-		for(int i=0;i<test.length;i++) {
-			int mask = (int) Math.pow(2, i);
-			if((index & mask) == mask)
-				subset.add(test[i]);
+	public int encode(List<Integer> subset) {
+		int id = 0;
+		for(int n : subset) {
+			id += Math.pow(2, n-1);
+		}
+		return id;
+	}
+	
+	public List<Integer> decode(int comboId, int n){
+		List<Integer> productIds = new ArrayList<>();
+		for(int i=0;i<n;i++) {
+			int mask = (int)Math.pow(2, i);
+			if((comboId & mask) == mask)
+				productIds.add(i+1);
 		}
 		
-		return subset;
+		return productIds;
 	}
+	
+	
+	
+	public void getSetWithIndex(int[] test, int index,
+			Map<Integer, List<Integer>> setCache, Map<Integer, Integer> setSumCache) {
+		List<Integer> subset = new ArrayList<>();
+		int sum = 0;
+		for(int i=0;i<test.length;i++) {
+			int mask = (int) Math.pow(2, i);
+			//System.out.printf("Index:%d\t Mask:%d\ti:%d\n",index, mask,i);
+			if((index & mask) == mask) {
+				subset.add(test[i]);
+				sum += test[i];
+			}
+		}
+		
+		setCache.put(index, subset);
+		setSumCache.put(index, sum);
+		
+	}
+	
+	public void getSetWithIndexOptimized(int[] productIds, int[] prices, int index,
+			Map<Integer, List<Integer>> setCache, Map<Integer, Integer> setSumCache) {
+		List<Integer> subset = new ArrayList<>();
+		int sum = 0;
+		
+		if(index >= 2) {
+			int mask = (int) (Math.pow(2, log2Floor(index)) - 1);
+			int prev = index & mask; // resetting the Msb
+			
+			subset.addAll(setCache.get(prev));
+			
+			int newIndex = log2Floor(index);
+			subset.add(productIds[newIndex]);
+			sum = setSumCache.get(prev) + prices[newIndex];
+			//System.out.println(".. hiting the cache.");
+			
+		}else {
+			if(index == 1) {
+				subset.add(productIds[0]);
+				sum += prices[0];
+			}
+		}
+		
+		setCache.put(index, subset);
+		setSumCache.put(index, sum);
+		
+	}
+	
+	static int log2Floor(int n) {
+    	double log2 = Math.log(n) / Math.log(2);
+    	int result = (int) Math.floor(log2);
+    	
+    	return result;
+    }
 
 }
